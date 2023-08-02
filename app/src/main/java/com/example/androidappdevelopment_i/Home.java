f9b9be0c0e2fad9fae44c6fbbb8e9dd40c5ad442
M.A. Asardeen <asardeenbse@gmail.com>
2023-08-02 15:25:47 +0530

    update easy method of java lines of code

--- app/src/main/java/com/example/androidappdevelopment_i/Home.java
+++ app/src/main/java/com/example/androidappdevelopment_i/Home.java
@@ -8,57 +8,63 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class Home extends AppCompatActivity implements View.OnClickListener {
+public class Home extends AppCompatActivity {
+//remove the implements
+//remove the getting value method
 
+    private EditText value1, value2;
+    private Button add, sub;
+    private TextView results;
 
-    private String inputValueNum1, inputValueNum2;
-
-    public void gettingValue(){
-        EditText num1 = findViewById(R.id.editNum1);
-        inputValueNum1 = num1.getText().toString();
-        EditText num2 =  findViewById(R.id.editNum2);
-        inputValueNum2 = num2.getText().toString();
-    }
-
-
-    @Override
-    public void onClick(View view) {
-
-        switch (view.getId()){
-            case R.id.btnAdd:
-               gettingValue();
-               int add = Integer.parseInt(inputValueNum1) + Integer.parseInt(inputValueNum2);
-               String resultsAdd = String.valueOf(add);
-               TextView textView1 = findViewById(R.id.txtOut);
-               textView1.setText(resultsAdd);
-                break;
-
-            case R.id.btnSub:
-                gettingValue();
-                int sub = Integer.parseInt(inputValueNum1) - Integer.parseInt(inputValueNum2);
-                String resultsSub = String.valueOf(sub);
-                TextView textView2 = findViewById(R.id.txtOut);
-                textView2.setText(resultsSub);
-                break;
-
-            default:
-                break;
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+         //on create methods above default
 
-        Button btnAdd = findViewById(R.id.btnAdd);
-        btnAdd.setOnClickListener(this);
+        //finding id and initialize
 
-        Button btnSub = findViewById(R.id.btnSub);
-        btnSub.setOnClickListener(this);
+        value1 = findViewById(R.id.editNum1);
+        value2 = findViewById(R.id.editNum2);
+        add = findViewById(R.id.btnAdd);
+        sub = findViewById(R.id.btnSub);
+        results= findViewById(R.id.txtOut);
 
+        //add button when click
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    add();
+            }
+        });
+
+        //sub button when click
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sub();
+            }
+        });
 
     }
 
 
+    private void add(){
+        Double num1 = Double.parseDouble(value1.getText().toString());
+        Double num2 = Double.parseDouble(value2.getText().toString());
+        Double res = num1 + num2;
+        results.setText("Sum of Two Values : " + String.valueOf(res));
+    }
+
+    private void sub(){
+        Double num1 = Double.parseDouble(value1.getText().toString());
+        Double num2 = Double.parseDouble(value2.getText().toString());
+        Double res = num1 - num2;
+        results.setText("Sub of Two Values : " + String.valueOf(res));
+    }
+
+
+
 }
+
